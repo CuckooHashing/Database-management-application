4f8c96bb9d470adeb4b595028bd9f0a272ddc461
Diana Anca <61456916+PancakeSort49@users.noreply.github.com>
2020-05-20 16:27:10 +0300

    Add files via upload

---
+++ lab1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.tittleBox = new System.Windows.Forms.TextBox();
+            this.countBox = new System.Windows.Forms.TextBox();
+            this.ratingBox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(648, 255);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 296);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(648, 254);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(705, 417);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(122, 52);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // tittleBox
+            // 
+            this.tittleBox.Location = new System.Drawing.Point(705, 93);
+            this.tittleBox.Name = "tittleBox";
+            this.tittleBox.Size = new System.Drawing.Size(262, 20);
+            this.tittleBox.TabIndex = 5;
+            this.tittleBox.Text = "Title";
+            this.tittleBox.Click += new System.EventHandler(this.tittleBox_Click);
+            // 
+            // countBox
+            // 
+            this.countBox.Location = new System.Drawing.Point(705, 156);
+            this.countBox.Name = "countBox";
+            this.countBox.Size = new System.Drawing.Size(262, 20);
+            this.countBox.TabIndex = 6;
+            this.countBox.Text = "Episode Count";
+            this.countBox.Click += new System.EventHandler(this.countBox_Click);
+            // 
+            // ratingBox
+            // 
+            this.ratingBox.Location = new System.Drawing.Point(705, 224);
+            this.ratingBox.Name = "ratingBox";
+            this.ratingBox.Size = new System.Drawing.Size(262, 20);
+            this.ratingBox.TabIndex = 7;
+            this.ratingBox.Text = "Rating";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(705, 289);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(82, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "It is a movie";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(705, 351);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(262, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.Text = "Manga ID";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(863, 417);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(122, 52);
+            this.deleteButton.TabIndex = 10;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(783, 507);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(122, 52);
+            this.updateButton.TabIndex = 11;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(732, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 17);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "StudiosAnime";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(732, 47);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(99, 17);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.Text = "ComposersOST";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(997, 565);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.ratingBox);
+            this.Controls.Add(this.countBox);
+            this.Controls.Add(this.tittleBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox tittleBox;
+        private System.Windows.Forms.TextBox countBox;
+        private System.Windows.Forms.TextBox ratingBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ lab1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using lab1;
+
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        private static SqlReader sqlReader;
+        private string whomst = "StudiosAnime";
+        private SqlConnection connection;
+
+        public Form1()
+        {
+            this.connection = new SqlConnection();
+            this.connection.ConnectionString = @"Data Source = USER\SQLEXPRESS01; Initial Catalog = Anime; Integrated Security = true";
+            InitializeComponent();
+        }
+        private void LoadQueries()
+        {
+            Dictionary<string, string> sqls = new Dictionary<string, string>();
+
+        }
+        private void PopulateParentGrid()
+        {
+            sqlReader = SqlReader.Create(whomst);
+            try
+            {
+                DataSet dataSet = new DataSet();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlReader.GetQuery("getParent"), connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+                dataAdapter.Fill(dataSet);
+                dataGridView1.ReadOnly = true;
+                dataGridView1.DataSource = dataSet.Tables[0];
+                dataGridView2.DataSource = null;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            object[] range = new object[6] { 1, 2, 3, 4, 5, 6 };
+            comboBox1.Items.AddRange(range);
+            PopulateParentGrid();
+        }
+
+        private void populateChildGrid()
+        {
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlReader.GetQuery("getChild") + dataGridView1.SelectedCells[0].Value.ToString(), connection);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            dataAdapter.Fill(dataSet);
+            dataGridView2.ReadOnly = true;
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(dataGridView1.CurrentCell.ColumnIndex != 0)
+            {
+                MessageBox.Show("Please select the ID field", "Error");
+                return;
+            }
+            try
+            {
+                populateChildGrid();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            
+        }
+
+        private void tittleBox_Click(object sender, EventArgs e)
+        {
+            tittleBox.Text = "";
+        }
+
+        private void countBox_Click(object sender, EventArgs e)
+        {
+            countBox.Text = "";
+        }
+
+        private string newAnime()
+        {
+            
+            string data = "";
+                //dataSet.Tables["Anime"].Rows.Add(new Object[] {,, checkBox1.Checked, count, Convert.ToInt32(comboBox1.SelectedValue), rating, Convert.ToInt32(dataGridView1.SelectedCells[0].Value) });
+                data += ",";
+                data += tittleBox.Text;
+                data += ",";
+                data += checkBox1.Checked;
+                data += ",";
+                data += countBox.Text;
+                data += ",";
+                data += comboBox1.SelectedValue;
+                data += ",";
+                data += ratingBox.Text;
+                data += ",";
+                data += dataGridView1.SelectedCells[0].Value;
+                data += ")";
+
+  
+            return data;
+        }
+
+        private string newOST()
+        {
+            string data = "";
+            data += ",";
+            data += tittleBox.Text;
+            data += ",";
+            data += countBox.Text;
+            data += ",";
+            data += ratingBox.Text;
+            data += ",";
+            data += dataGridView1.SelectedCells[0].Value;
+            data += ")";
+            return data;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string newElement = "";
+            if (whomst == "StudiosAnime")
+                newElement = newAnime();
+            else
+                newElement = newOST();
+            try
+            {
+                connection.Open();
+                SqlCommand command1 = new SqlCommand(sqlReader.GetQuery("countChild"), connection);
+                Random random = new Random();
+                int result = (int)command1.ExecuteScalar();
+                result += random.Next(result, result * 7);
+                connection.Close();
+
+                connection.Open();
+                string query = sqlReader.GetQuery("addChild") + result.ToString() + newElement;
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+                populateChildGrid();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.CurrentCell.ColumnIndex != 0)
+            {
+                MessageBox.Show("Please select the ID field of the object that you want to delete", "Error");
+                return;
+            }
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlReader.GetQuery("deleteChild") + dataGridView1.SelectedCells[0].Value.ToString(), connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+                populateChildGrid();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.CurrentCell.ColumnIndex != 0)
+            {
+                MessageBox.Show("Please select the ID field of the object that you want to update", "Error");
+                return;
+            }
+            try
+            {
+                string newElement = "";
+                if (whomst == "StudiosAnime")
+                    newElement = newAnime();
+                else
+                    newElement = newOST();
+                int id = Convert.ToInt32(dataGridView1.SelectedCells[0].Value);
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlReader.GetQuery("deleteChild") + id.ToString(), connection);
+                command.ExecuteNonQuery();
+                string query = sqlReader.GetQuery("addChild") + id.ToString() + newElement;
+                SqlCommand command2 = new SqlCommand(query, connection);
+                command2.ExecuteNonQuery();
+                connection.Close();
+                populateChildGrid();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                whomst = "StudiosAnime";
+                PopulateParentGrid();
+                checkBox1.Visible = true;
+                comboBox1.Text = "Manga ID";
+                tittleBox.Text = "Title";
+                countBox.Text = "Episode Count";
+                ratingBox.Text = "Rating";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                whomst = "ComposersOST";
+                PopulateParentGrid();
+                checkBox1.Visible = false;
+                comboBox1.Text = "Character ID";
+                tittleBox.Text = "Length";
+                countBox.Text = "Genre";
+                ratingBox.Text = "Album";
+            }
+        }
+
+    }
+}
